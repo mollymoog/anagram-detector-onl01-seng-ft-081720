85aa92b1e37da91c28cb552794a60b597841825f
molly mk <mmkover@gmail.com>
2020-08-30 23:59:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match (matches)
     letters = word.split("")  
     matches.each do |words|
-      if words.split("")
+      if words.split("").sort == letters.sort
   end
  
   
