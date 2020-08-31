f2b9f6bc0b0bcd045472506788cb6bd598a6793a
molly mk <mmkover@gmail.com>
2020-08-31 00:13:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+ attr_accessor :word
+ 
+ def initialize (word)
+    @word = word
+  end
+  
+  def match (matches)
+    new_word = []
+    letters = word.split("")  
+    matches.collect do |words|
+      if words.split("").sort == letters.sort
+        new_word << words
+      end
+    end
+    new_word
+  end
+end
